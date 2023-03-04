968899e1fa9c511fab75a99906a39b8e036545dc
Claudio Cruz <claudiocruz1989@gmail.com>
2023-03-04 13:06:40 +0000

    Update axios defaults url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://taskify-2023.herokuapp.com'
-// axios.defaults.baseURL = 'https://taskify-app.herokuapp.com/'
+axios.defaults.baseURL = 'https://taskify-app-2023.herokuapp.com'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
