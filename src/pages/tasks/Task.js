da7192e2b6c4384419dc4340b7c3f4b7fe81086e
Claudio Cruz <claudiocruz1989@gmail.com>
2023-06-05 04:02:22 +0000

    Add handleDelete function to Task list

--- src/pages/tasks/Task.js
+++ src/pages/tasks/Task.js
@@ -1,35 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Table, Dropdown } from 'react-bootstrap';
 // import { useCurrentUser } from "../../contexts/CurrentUserContext";
-// import { useHistory } from "react-router-dom";
-// import { axiosRes } from "../../api/axiosDefaults";
+//import { useHistory } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Task = (props) => {
   const {
+    id,
     task,
     due_date,
     priority,
     category,
   } = props;
-  
 
+  const [deleted, setDeleted] = useState(false);
   // const currentUser = useCurrentUser();
-  // const history = useHistory();
-  
+  //const history = useHistory();
+
   /*const handleEdit = () => {
     history.push(`/tasks/${id}/edit`);
   };*/
 
-  /*const handleDelete = async () => {
+  const handleDelete = async () => {
     try {
       await axiosRes.delete(`/tasks/${id}/`);
-      history.goBack();
+      setDeleted(true);
     } catch (err) {
+      // Handle error
     }
-  };*/
-  
-    return (
-      <div>
+  };
+
+  if (deleted) {
+    return null;
+  }
+
+  return (
+    <div>
       <Table striped bordered hover>
         <tbody>
           <tr>
@@ -38,18 +44,18 @@ const Task = (props) => {
             <td>{category}</td>
             <td>{priority}</td>
             <td className="text-end d-flex justify-content-end">
-            <Dropdown drop="down">
+              <Dropdown drop="down">
                 <Dropdown.Toggle variant="primary" size="sm">
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                <Dropdown.Item>
-                <i className="fa-solid fa-check fa-lg"></i> Done
-                </Dropdown.Item>
                   <Dropdown.Item>
-                  <i className="fa-solid fa-pen-to-square"></i> Edit
+                    <i className="fa-solid fa-check fa-lg"></i> Done
                   </Dropdown.Item>
                   <Dropdown.Item>
-                  <i className="fa-solid fa-trash-can"></i> Delete
+                    <i className="fa-solid fa-pen-to-square"></i> Edit
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={handleDelete}>
+                    <i className="fa-solid fa-trash-can"></i> Delete
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
@@ -58,7 +64,7 @@ const Task = (props) => {
         </tbody>
       </Table>
     </div>
-    
+
   )
 }
 
