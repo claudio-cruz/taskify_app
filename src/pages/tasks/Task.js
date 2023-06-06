d9071da336460b2645759265e282c825715ddaaf
Claudio Cruz <claudiocruz1989@gmail.com>
2023-06-06 11:21:32 +0000

    Add a Form.Check to the Task.js and update the task list display

--- src/pages/tasks/Task.js
+++ src/pages/tasks/Task.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { Table, Dropdown } from 'react-bootstrap';
-// import { useCurrentUser } from "../../contexts/CurrentUserContext";
-//import { useHistory } from "react-router-dom";
+import { Container, Row, Col, Dropdown, Form } from 'react-bootstrap';
+import Styles from '../../styles/Task.module.css'
 import { axiosRes } from "../../api/axiosDefaults";
 
 const Task = (props) => {
@@ -9,17 +8,14 @@ const Task = (props) => {
     id,
     task,
     due_date,
+    description,
+    completed,
     priority,
     category,
+    onUpdateComplete,
   } = props;
 
   const [deleted, setDeleted] = useState(false);
-  // const currentUser = useCurrentUser();
-  //const history = useHistory();
-
-  /*const handleEdit = () => {
-    history.push(`/tasks/${id}/edit`);
-  };*/
 
   const handleDelete = async () => {
     try {
@@ -34,37 +30,65 @@ const Task = (props) => {
     return null;
   }
 
-  return (
-    <div>
-      <Table striped bordered hover>
-        <tbody>
-          <tr>
-            <td>{task}</td>
-            <td>{due_date}</td>
-            <td>{category}</td>
-            <td>{priority}</td>
-            <td className="text-end d-flex justify-content-end">
-              <Dropdown drop="down">
-                <Dropdown.Toggle variant="primary" size="sm">
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item>
-                    <i className="fa-solid fa-check fa-lg"></i> Done
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <i className="fa-solid fa-pen-to-square"></i> Edit
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={handleDelete}>
-                    <i className="fa-solid fa-trash-can"></i> Delete
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+  const handleComplete = async () => {
+    try {
+      // Toggle the task completion status in the backend
+      const updatedCompleted = !completed;
+      await axiosRes.patch(`/tasks/${id}/`, { completed: updatedCompleted });
 
+      // Call the onUpdateComplete prop to update the completion status in the parent component
+      if (onUpdateComplete) {
+        onUpdateComplete(id, updatedCompleted);
+      }
+    } catch (err) {
+      // Handle error
+    }
+  };
+
+  console.log(completed)
+
+  return (
+    <Container className={Styles.TaskContainer}>
+      <Row>
+        <Col xs={9} md={10}>
+          <h5 className='font-weight-bold'>{task}</h5>
+        </Col>
+        <Col xs={1}>
+          <Form.Check
+            className="form-check checkbox-lg"
+            aria-label="option 1"
+            defaultChecked={completed}
+            onClick={handleComplete}
+          />
+        </Col>
+        <Col xs={1}>
+          <Dropdown drop="down">
+            <Dropdown.Toggle variant="primary" size="sm">
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <i className="fa-solid fa-pen-to-square"></i> Edit
+              </Dropdown.Item>
+              <Dropdown.Item onClick={handleDelete}>
+                <i className="fa-solid fa-trash-can"></i> Delete
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+      {description && (
+        <Row>
+          <Col>
+            <p>{description}</p>
+          </Col>
+        </Row>
+      )}
+      <Row>
+        <Col xs={5}><p>{due_date}</p></Col>
+        <Col><p>{category}</p></Col>
+        <Col xs={3}><p>{priority}</p></Col>
+      </Row>
+    </Container>
   )
 }
 
