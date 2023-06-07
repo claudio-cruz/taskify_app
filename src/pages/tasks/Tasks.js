10c080fff025dfcaf2159ee24987948d34f341ad
Claudio Cruz <claudiocruz1989@gmail.com>
2023-06-07 19:51:34 +0000

    Add a tab filter to the tasks list

--- src/pages/tasks/Tasks.js
+++ src/pages/tasks/Tasks.js
@@ -9,11 +9,10 @@ import { useLocation } from "react-router";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
-import { Button, Row, Col, Spinner  } from 'react-bootstrap';
+import { Button, Row, Col, Spinner, Tab, Nav } from 'react-bootstrap';
 
 
 function TaskList({ filter = "" }) {
-
   const [tasks, setTasks] = useState({ results: [], next: null });
   const currentUser = useCurrentUser();
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -27,7 +26,7 @@ function TaskList({ filter = "" }) {
         setTasks(data);
         setHasLoaded(true);
       } catch (err) {
-
+        // Handle error
       }
     };
 
@@ -41,10 +40,8 @@ function TaskList({ filter = "" }) {
     };
   }, [filter, query, pathname, currentUser]);
 
-
   return (
     <Container>
-
       <div className="d-flex justify-content-center p-3">
         <Form onSubmit={(event) => event.preventDefault()}>
           <div className="input-group">
@@ -63,38 +60,162 @@ function TaskList({ filter = "" }) {
         </Form>
       </div>
 
-      {hasLoaded ? (
-        <>
-          {tasks.results.length ? (
-            <InfiniteScroll
-              children={tasks.results.map((task) => (
-                <Task key={task.id} {...task} setTasks={setTasks} />
-              ))}
-              dataLength={tasks.results.length}
-              loader={<Spinner animation="border" variant="dark"/>}
-              hasMore={!!tasks.next}
-              next={() => fetchMoreData(tasks, setTasks)}
-              endMessage={<p>No more tasks.</p>}
-            />
-          ) : (
-            // if no results
-            <Container >
-              <p>No results</p>
-            </Container>
-          )}
-        </>
-      ) : (
-        // loding data
-        <Container>
-          <Spinner animation="border" variant="dark" text-align="center" />
-        </Container>
-      )}
+      <Tab.Container id="task-filters" defaultActiveKey="all">
+        <Nav variant="tabs" className="justify-content-center mb-3">
+          <Nav.Item>
+            <Nav.Link eventKey="all" className={styles.TabNavLink}>All</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="done" className={styles.TabNavLink}>Done</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="undone" className={styles.TabNavLink}>Undone</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="high" className={styles.TabNavLink}>High</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="medium" className={styles.TabNavLink}>Medium</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="low" className={styles.TabNavLink}>Low</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          <Tab.Pane eventKey="all">
+            {hasLoaded ? (
+              <>
+                {tasks.results.length ? (
+                  <InfiniteScroll
+                    children={tasks.results.map((task) => (
+                      <Task key={task.id} {...task} setTasks={setTasks} />
+                    ))}
+                    dataLength={tasks.results.length}
+                    loader={<Spinner animation="border" variant="dark" />}
+                    hasMore={!!tasks.next}
+                    next={() => fetchMoreData(tasks, setTasks)}
+                    endMessage={<p>No more tasks.</p>}
+                  />
+                ) : (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+          <Tab.Pane eventKey="done">
+            {hasLoaded ? (
+              <>
+                {tasks.results
+                  .filter((task) => task.completed)
+                  .map((task) => (
+                    <Task key={task.id} {...task} setTasks={setTasks} />
+                  ))}
+                {!tasks.results.some((task) => task.completed) && (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+          <Tab.Pane eventKey="undone">
+            {hasLoaded ? (
+              <>
+                {tasks.results
+                  .filter((task) => !task.completed)
+                  .map((task) => (
+                    <Task key={task.id} {...task} setTasks={setTasks} />
+                  ))}
+                {!tasks.results.some((task) => !task.completed) && (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+          <Tab.Pane eventKey="high">
+            {hasLoaded ? (
+              <>
+                {tasks.results
+                  .filter((task) => task.priority === "high")
+                  .map((task) => (
+                    <Task key={task.id} {...task} setTasks={setTasks} />
+                  ))}
+                {!tasks.results.some((task) => task.priority === "high") && (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+          <Tab.Pane eventKey="medium">
+            {hasLoaded ? (
+              <>
+                {tasks.results
+                  .filter((task) => task.priority === "medium")
+                  .map((task) => (
+                    <Task key={task.id} {...task} setTasks={setTasks} />
+                  ))}
+                {!tasks.results.some((task) => task.priority === "medium") && (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+          <Tab.Pane eventKey="low">
+            {hasLoaded ? (
+              <>
+                {tasks.results
+                  .filter((task) => task.priority === "low")
+                  .map((task) => (
+                    <Task key={task.id} {...task} setTasks={setTasks} />
+                  ))}
+                {!tasks.results.some((task) => task.priority === "low") && (
+                  <Container>
+                    <p>No results</p>
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Spinner animation="border" variant="dark" text-align="center" />
+              </Container>
+            )}
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
+
 
       <Container>
         <Row className="justify-content-end">
           <Col xs="auto">
             <Button variant="link">
-
               <Link
                 to="/tasks/create"
                 className={styles.NavLink}
@@ -106,7 +227,6 @@ function TaskList({ filter = "" }) {
           </Col>
         </Row>
       </Container>
-
     </Container>
   );
 }
