b6261d8d38899953f05ed52c55470f4482bea7cf
Claudio Cruz <claudiocruz1989@gmail.com>
2023-06-08 19:22:21 +0000

    Add a useEffect function to the NavBar component to handle clicks outside the nav drop-down menu.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
@@ -14,6 +14,22 @@ const NavBar = () => {
 
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const [expanded, setExpanded] = useState(false)
+    const ref = useRef(null)
+    
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+          if (ref.current && !ref.current.contains(event.target)) {
+            setExpanded(false);
+          }
+        };
+    
+        document.addEventListener('mouseup', handleClickOutside);
+        return () => {
+          document.removeEventListener('mouseup', handleClickOutside);
+        };
+      }, [ref]);
 
     const handleSignOut = async () => {
         try {
@@ -90,7 +106,7 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container fluid  className="p-0">
 
                 <NavLink to="/">
@@ -105,7 +121,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                ref={ref}
+                    onClick={() => setExpanded(!expanded)}
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
 
